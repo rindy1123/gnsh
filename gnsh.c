7eb9c7a1ecb3aa34bc7e26aa4a570028b25d72bf
rindy1123 <genryusinbaou@gmail.com>
2021-05-02 18:16:19 +0900

    execute command

--- gnsh.c
+++ gnsh.c
@@ -25,18 +25,42 @@ main(int argc, char *argv[])
       if (strcmp(line, "\0") == 0)
         continue;
 
-      char *bin_path = (char *)malloc(strlen(line) + strlen(BIN_PATH) + 1);
-      char **command_arg = NULL;
-      sprintf(bin_path, "%s/%s", BIN_PATH, line);
+      char *command;
+      command = line;
+
+      char *bin_path = (char *)malloc(strlen(command) + strlen(BIN_PATH) + 1);
+      sprintf(bin_path, "%s/%s", BIN_PATH, command);
+      char **command_args = (char **)malloc((strlen(bin_path) + 30) * sizeof(char *));
+      // char *command_args[3] = {bin_path, "-a", NULL};
+      command_args[0] = bin_path;
+      command_args[1] = "-a";
+      command_args[2] = NULL;
       if (access(bin_path, X_OK) == 0)
       {
-        execv(bin_path, command_arg);
-        fprintf(out, "%s\n", bin_path);
-        free(bin_path);
-        continue;
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+          fprintf(stderr, "error occurs");
+          exit(1);
+        }
+        else if (pid == 0)
+        {
+          if (execv(bin_path, command_args) == -1)
+          {
+            fprintf(stderr, "error occurs");
+            exit(1);
+          }
+        }
+        else
+        {
+          waitpid(pid, NULL, 0);
+          fprintf(out, "%s\n", bin_path);
+          free(bin_path);
+          continue;
+        }
       }
-      char *usr_bin_path = (char *)malloc(strlen(line) + strlen(USR_BIN_PATH) + 1);
-      sprintf(usr_bin_path, "%s/%s", USR_BIN_PATH, line);
+      char *usr_bin_path = (char *)malloc(strlen(command) + strlen(USR_BIN_PATH) + 1);
+      sprintf(usr_bin_path, "%s/%s", USR_BIN_PATH, command);
       if (access(usr_bin_path, X_OK) == 0)
       {
         fprintf(out, "%s\n", usr_bin_path);
@@ -44,13 +68,13 @@ main(int argc, char *argv[])
         free(usr_bin_path);
         continue;
       }
-      if (strcmp(line, "exit") == 0)
+      if (strcmp(command, "exit") == 0)
       {
         free(usr_bin_path);
         free(bin_path);
         return 0;
       }
-      fprintf(stderr, "gnsh: Unknown command: %s\n", line);
+      fprintf(stderr, "gnsh: Unknown command: %s\n", command);
       free(usr_bin_path);
       free(bin_path);
     }
