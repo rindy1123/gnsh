2324e9c80c9bd7d9ea4f62e5060def20faef4194
rindy1123 <genryusinbaou@gmail.com>
2021-05-02 12:00:09 +0900

    feat: read command

--- gnsh.c
+++ gnsh.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#define BASE_PATH "/bin"
+#include <unistd.h>
+#define BIN_PATH "/bin"
+#define USR_BIN_PATH "/usr/bin"
 
 int
 main(int argc, char *argv[])
 {
   FILE *in;
   FILE *out;
-  char *path = BASE_PATH;
   if (argc == 1)
   {
     in = stdin;
@@ -19,9 +20,27 @@ main(int argc, char *argv[])
     {
       fprintf(out, "gnsh> ");
       getline(&line, &linecap, in);
-      if (strcmp(line, "exit\n") == 0)
-        return 0;
-      fprintf(out, "%s/%s", path, line);
+      // remove new line char
+      line[strcspn(line, "\n")] = 0;
+      if (strcmp(line, "\0") == 0)
+        continue;
+      char *bin_path = (char *)malloc(strlen(line) + strlen(BIN_PATH) + 1);
+      sprintf(bin_path, "%s/%s", BIN_PATH, line);
+      if (access(bin_path, X_OK) == 0)
+        fprintf(out, "%s\n", bin_path);
+      else
+      {
+        char *usr_bin_path = (char *)malloc(strlen(line) + strlen(USR_BIN_PATH) + 1);
+        sprintf(usr_bin_path, "%s/%s", USR_BIN_PATH, line);
+        if (access(usr_bin_path, X_OK) == 0)
+          fprintf(out, "%s\n", usr_bin_path);
+        else
+        {
+          if (strcmp(line, "exit") == 0)
+            return 0;
+          fprintf(stderr, "gnsh: Unknown command: %s\n", line);
+        }
+      }
     }
   }
 
